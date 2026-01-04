7be0ce47ec206a76b7100060198412b50e7e2c77
Kevin Moreau <moreau210@gmail.com>
2026-01-04 11:47:10 +0100

    Fix Blob upload: add random suffix to prevent conflicts

--- src/actions/upload.ts
+++ src/actions/upload.ts
@@ -18,6 +18,7 @@ export async function uploadImage(formData: FormData): Promise<string> {
         const blob = await put(file.name, file, {
             access: "public",
             token: token,
+            addRandomSuffix: true,
         });
 
         return blob.url;
